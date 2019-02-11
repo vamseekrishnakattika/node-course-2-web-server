99b88afdad1ad17ff1abefff6c6d6702e97b5f47
Vamseekrishna Kattika <vamseekrishna.kattika@gmail.com>
2019-02-11 12:31:24 -0600

    Add project page

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use((req,res,next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs')
-});
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs')
+// });
 
 hbs.registerHelper('getCurrentYear',() => {
   return new Date().getFullYear()
@@ -54,6 +54,12 @@ app.get('/',(req,res) => {
   res.render('home.hbs',{
     pageTitile: 'Home Page',
     welocmeMessage: 'Welcome to Home Page'
+  });
+});
+
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs',{
+    pageTitile:'Projetcs'
   });
 });
 
